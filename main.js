93174d3a52309dac0d86ea722efacf2d444d0453
Guilherme Tófoli <guitofoli1806@gmail.com>
2025-04-15 21:36:49 -0300

    Adicionado função para excluir e adicionado o console.log

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function salvarUser(){
 function criaLista(){
     let tabela = "<tr><th>Nome</th><th></th></tr>";
     for(let i = 0; i<=(dadosLista.length-1); i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning'>Editar</button><button class='btn btn-danger'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById("tabela").innerHTML = tabela;
     }
 }
@@ -43,4 +43,6 @@ function criaLista(){
 // Função para excluir nome da lista
 function excluir(i){
     dadosLista.splice ((i-1), 1);
+    document.getElementById("tabela").deleteRow(i);
+    console.log("dadosLista");
 }
